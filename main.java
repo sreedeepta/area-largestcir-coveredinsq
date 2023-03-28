1ec3d5fe95d8b1b00b0df121570f18d6459b1ffc
Sreedeepta <78677605+sreedeepta@users.noreply.github.com>
2023-03-28 10:39:54 +0530

    Create main.java

---
+++ main.java
@@ -0,0 +1,13 @@
+class Program10
+{
+    public void calculate(int s)
+    {
+        double r, a;
+        r=s/2.0;
+        a=(22/7.0)*(r*r);
+        
+        System.out.println("Area = "+a);
+    }
+    
+}
+
